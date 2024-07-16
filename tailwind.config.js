da4938b71c292f8df8dce8be4a30b3f032e173fc
Arnold <zhanga8866@gmail.com>
2024-07-17 01:18:17 +0930

    v2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 export default {
-   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-   darkMode: false, // or 'media' or 'class'
+   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+   darkMode: 'media', // or 'media' or 'class'
    theme: {
       extend: {
          keyframes: {
